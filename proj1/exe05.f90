77949e171aca698d5430f0c4bffc6333a1fc3b29
guilherme <guilherme@gac.guilherme>
2016-12-10 01:47:21 -0200

    Update - 10/12

---
+++ proj1/exe05.f90
@@ -0,0 +1,25 @@
+
+PROGRAM exe05
+!
+IMPLICIT NONE
+!
+INTEGER                 ::  N,i,j
+INTEGER,DIMENSION(1:100)   ::  vec
+!
+i   =   1
+READ    *, N
+!
+!! CALCULATE FOR BASE 2 !!
+DO WHILE (N .GT. 1)
+    vec(i)  =   MOD(N,2)
+    N       =   N/2
+    i       =   i+1
+END DO
+!
+WRITE (*,10,ADVANCE='no') 1
+DO j=i-1,1,-1
+    10 FORMAT(3i0)
+    WRITE (*,10,ADVANCE='no') vec(j) 
+END DO
+!
+END PROGRAM exe05
---
+++ proj1/script05.sh
@@ -0,0 +1,3 @@
+
+gfortran -o exe05 exe05.f90
+./exe05
