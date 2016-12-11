00f399b38f1a8cf66d2580889c33fd0060144a90
guilherme <guilherme@gac.guilherme>
2016-12-11 20:43:27 -0200

    Update - 11/12

--- proj1/exe05.f90
+++ proj1/exe05.f90
@@ -8,7 +8,9 @@ INTEGER,DIMENSION(1:100)   ::  vec
 !
 i   =   1
 READ    *, N
-!
+
+
+
 !! CALCULATE FOR BASE 2 !!
 DO WHILE (N .GT. 1)
     vec(i)  =   MOD(N,2)
@@ -16,10 +18,18 @@ DO WHILE (N .GT. 1)
     i       =   i+1
 END DO
 !
+
+
+
 WRITE (*,10,ADVANCE='no') 1
 DO j=i-1,1,-1
     10 FORMAT(3i0)
     WRITE (*,10,ADVANCE='no') vec(j) 
 END DO
-!
+
+
+
+
+
 END PROGRAM exe05
+
